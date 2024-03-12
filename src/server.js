2a7566715f977ed48400c02024cda83a436c42d6
mohankumar003-1 <smohankumar018@gmail.com>
2024-03-12 17:41:14 +0530

    delete feature added

--- src/server.js
+++ src/server.js
@@ -10,12 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.get('/', (req, res) => {
     const indexPath = path.join(__dirname, '/public/login.html');
     res.sendFile(indexPath);
@@ -32,25 +30,40 @@ app.get('/viewImages', async (req, res) => {
         const query = db.format(selectQuery);
         console.log('SQL Query:', query);
 
-        // Use a Promise to await the database query
         const [results] = await db.query(selectQuery);
 
         console.log(results);
-        // Send the response as JSON
         res.json({ data: results, message: 'Hello from /viewImages' });
     } catch (error) {
         console.error('Error during image retrieval:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+app.delete('/deleteImage/:id', async (req, res) => {
+    const imageId = req.params.id;
 
-// Error handling
+    try {
+        const deleteQuery = 'DELETE FROM images WHERE id = ?';
+        const query = db.format(deleteQuery, [imageId]);
+        console.log('SQL Query:', query);
+
+        const [result] = await db.query(deleteQuery, [imageId]);
+
+        if (result.affectedRows > 0) {
+            res.json({ message: 'Image deleted successfully' });
+        } else {
+            res.status(404).json({ error: 'Image not found' });
+        }
+    } catch (error) {
+        console.error('Error during image deletion:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running in ${PORT}`);
 });
